ae3538e2390afcf1ee2e7ca34e8a66a2b8b147ae
Benjamin Call <llacneb@prodatakey.com>
2019-07-16 14:46:09 -0600

    testing netlify reloading

--- src/components/main.js
+++ src/components/main.js
@@ -15,7 +15,7 @@ export default class Main extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-            There is going to be a lot of different types of calculator functions at some point.
+            There is going to be a lot of different types of calculator functions at some point...
           </p>
           <button onClick={() => this.setState({count: this.state.count + 1})}> Clicker</button>
           <div> {this.state.count} </div>
