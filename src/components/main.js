a0ee1dc51143532afbc63f0c2618b96ca9f45b71
Benjamin Call <llacneb@prodatakey.com>
2019-07-16 14:40:12 -0600

    First commit and initing

---
+++ src/components/main.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import '../styles/main.css';
+
+export default class Main extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            There is going to be a lot of different types of calculator functions at some point.
+          </p>
+          <button onClick={() => this.setState({count: this.state.count + 1})}> Clicker</button>
+          <div> {this.state.count} </div>
+        </header>
+      </div>
+    );
+  }
+}
